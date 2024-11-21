8f0338949d08fd0a9d12064dbd7aa33045cb8dd5
Jordan Fonseca <jordan.fonseca@agicap.com>
2024-11-21 08:40:16 +0100

    chore: configure eslint

--- eslint.config.js
+++ eslint.config.js
@@ -3,8 +3,18 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import stylistic from '@stylistic/eslint-plugin'
+import stylisticTs from '@stylistic/eslint-plugin-ts'
+import stylisticJsx from '@stylistic/eslint-plugin-jsx'
 
 export default tseslint.config(
+  stylistic.configs.customize({
+    indent: 2,
+    quotes: 'single',
+    semi: false,
+    jsx: true,
+
+  }),
   { ignores: ['dist'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
@@ -16,6 +26,8 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      '@stylistic/ts': stylisticTs,
+      '@stylistic/jsx': stylisticJsx,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -23,6 +35,7 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      '@stylistic/jsx-one-expression-per-line': ['error', { allow: 'single-line' }],
     },
   },
 )
